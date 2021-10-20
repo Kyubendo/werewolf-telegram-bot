b65b4d3b7b98fa02760a1f533314ad7b153dce59
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-20 18:21:23 +0300

    Добавил Недотёпа, Люкана и сообщение при начале дня

--- src/Roles/Villagers/Cursed.ts
+++ src/Roles/Villagers/Cursed.ts
@@ -4,5 +4,5 @@ export class Cursed extends VillagerBase {
     roleName = 'Проклятый';
     startMessageText = 'Ты Проклятый! Сейчас ты обычный смертный, ' +
         'но если волки выберут тебя съесть, ты станешь одним из них.';
-    weight = 0;
+    weight = -3;
 }
