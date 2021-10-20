dbde6049a8ff6acd8b37d36643c012832331741d
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-20 15:44:37 +0300

    Добавил Недотёпа, Люкана и сообщение при начале дня

--- src/Roles/Wolfs/Wolf.ts
+++ src/Roles/Wolfs/Wolf.ts
@@ -1,23 +1,19 @@
 import {WolfBase} from "./WolfBase";
-import {playersButtons} from "../../Game/playersButtons";
-import {Player} from "../../Player/Player";
+// import {playersButtons} from "../../Game/playersButtons";
+// import {Player} from "../../Player/Player";
 
 export class Wolf extends WolfBase {
     roleName = 'Wolf';
     startMessageText = 'Ты волк. Скушай всё село.';
     weight = -10;
 
-    nightAction = () => {
-        Wolf.bot.sendMessage(
-            this.player.id,
-            'Кого ты хочешь съесть?',
-            {
-                reply_markup: playersButtons(Wolf.game.players, true, this.player)
-            }
-        )
-    }
-
-    handleChoice = (chosenPlayer?: Player) => {
-        console.log(chosenPlayer);
-    }
+    // nightAction = () => {
+    //     Wolf.bot.sendMessage(
+    //         this.player.id,
+    //         'Кого ты хочешь съесть?',
+    //         {
+    //             reply_markup: playersButtons(Wolf.game.players, true, this.player)
+    //         }
+    //     )
+    // }
 }
