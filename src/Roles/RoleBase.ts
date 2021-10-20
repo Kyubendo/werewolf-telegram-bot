97159256b7db3d33d6be48f75cbbf233198d5ea2
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-20 17:34:37 +0300

    Добавил Недотёпа, Люкана и сообщение при начале дня

--- src/Roles/RoleBase.ts
+++ src/Roles/RoleBase.ts
@@ -1,6 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
 import {Game} from "../Game/Game";
 import {Player} from "../Player/Player";
+import {Wolf} from "./Wolfs/Wolf";
 
 export abstract class RoleBase {
     constructor(readonly player: Player) {
@@ -13,6 +14,29 @@ export abstract class RoleBase {
     abstract readonly startMessageText: string
 
     readonly handleChoice?: (choice?: string) => void
+    handleDeath = (killer?: Player) => {
+        if (killer === undefined) {
+
+        }
+        else if (killer.role instanceof Wolf) {
+            RoleBase.bot.sendMessage(
+                RoleBase.game.chatId,
+                `НомномНОМномНОМНОМном... ${this.player.name} съели заживо! 
+            ${this.player.name} был(а) ${this.player.role?.roleName}.`
+            )
+        }
+
+    }
+
+    targetPlayer?: Player
+    choiceMsgId?: number
+
+    choiceMsgEditText = () => {
+        RoleBase.bot.editMessageText(
+            `Выбор принят: ${this.targetPlayer?.name}.`,
+            {message_id: this.choiceMsgId, chat_id: this.player.id}
+        )
+    }
 
     readonly action?: () => void
     readonly actionResolve?: () => void
