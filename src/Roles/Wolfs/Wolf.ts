f6eb376403af69754cb3712687da6e0a465ab201
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-20 18:21:37 +0300

    Добавил Недотёпа, Люкана и сообщение при начале дня

--- src/Roles/Wolfs/Wolf.ts
+++ src/Roles/Wolfs/Wolf.ts
@@ -1,19 +1,33 @@
 import {WolfBase} from "./WolfBase";
-// import {playersButtons} from "../../Game/playersButtons";
-// import {Player} from "../../Player/Player";
+import {playersButtons} from "../../Game/playersButtons";
+import {Player} from "../../Player/Player";
+import {findPlayer} from "../../Game/findPlayer";
 
 export class Wolf extends WolfBase {
-    roleName = 'Волк';
+    roleName = 'Wolf';
     startMessageText = 'Ты волк. Скушай всё село.';
     weight = -10;
 
-    // nightAction = () => {
-    //     Wolf.bot.sendMessage(
-    //         this.player.id,
-    //         'Кого ты хочешь съесть?',
-    //         {
-    //             reply_markup: playersButtons(Wolf.game.players, true, this.player)
-    //         }
-    //     )
-    // }
+    action = () => {
+        if (Wolf.game.stage !== 'night') return;
+        Wolf.bot.sendMessage(
+            this.player.id,
+            'Кого ты хочешь съесть?',
+            {
+                reply_markup: playersButtons(Wolf.game.players, true,
+                    Wolf.game.players.filter(wolf => wolf.role instanceof Wolf))
+            }
+        ).then(msg => this.choiceMsgId = msg.message_id)
+    }
+
+    actionResolve = () => {
+        if (Wolf.game.stage !== 'night' || !this.targetPlayer) return;
+        //this.targetPlayer.isAlive = false;
+    }
+
+    handleChoice = (choice?: string) => {
+        this.targetPlayer = findPlayer(choice, Wolf.game.players);
+        if (!this.targetPlayer) return;
+        this.choiceMsgEditText();
+    }
 }
