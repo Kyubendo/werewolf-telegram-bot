bc76de2df21caa13ba717fdea624c06868e3fedb
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-20 12:38:26 +0300

    Добавил Недотёпа, Люкана и сообщение при начале дня

--- src/Roles/Wolfs/Lycan.ts
+++ src/Roles/Wolfs/Lycan.ts
@@ -1,12 +1,9 @@
 import {WolfBase} from "./WolfBase";
-import {Game} from "../../Game/Game";
-import {playerList} from "../../Game/playerList";
-import {Player} from "../../Player/Player";
 import {RoleBase} from "../RoleBase";
 
 export class Lycan extends WolfBase {
     roleName = 'Ликан';
     startMessageText = 'Ты Ликан! Ты волк, но провидец видит тебя селянином :)';
-    weight = RoleBase.game.players.map(player => player.role?.roleName === 'Seer'
-        || player.role?.roleName === 'Fool') ? 12 : 10; // sets the weight depending on seer or fool presence
+    weight = RoleBase.game.players.find(player => player.role?.roleName === 'Seer' ||
+        player.role?.roleName === 'Fool') ? 12 : 10; // Sets the weight of lycan depending on seer or fool presence
 }
