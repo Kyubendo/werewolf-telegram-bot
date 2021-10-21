4abe83a182c677e41a5fde8fb4286eb73ca9e61e
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-21 12:04:45 +0300

    Added Mason to index and roleAssign

--- src/Roles/index.ts
+++ src/Roles/index.ts
@@ -5,6 +5,7 @@ import {ClumsyGuy} from "./Villagers/ClumsyGuy";
 import {Cursed} from "./Villagers/Cursed";
 import {Traitor} from "./Villagers/Traitor";
 import {WoodMan} from "./Villagers/WoodMan";
+import {Mason} from "./Villagers/Mason";
 
 import {Wolf} from "./Wolfs/Wolf";
 import {Lycan} from "./Wolfs/Lycan";
@@ -12,7 +13,7 @@ import {Lycan} from "./Wolfs/Lycan";
 import {Suicide} from "./Others/Suicide";
 
 export {
-    Seer, Fool, Villager, ClumsyGuy, Cursed, Traitor, WoodMan,
+    Seer, Fool, Villager, ClumsyGuy, Cursed, Traitor, WoodMan, Mason,
     Wolf, Lycan,
     Suicide
 }
