6bde084068c4a7a7de43df8ba1ccd8fbba75a12c
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-24 10:19:17 +0300

    Очевидец

--- src/Roles/Villagers/Beholder.ts
+++ src/Roles/Villagers/Beholder.ts
@@ -8,8 +8,8 @@ export class Beholder extends Villager {
     private seers?: Player[] = Beholder.game.players
         .filter(player => player.role instanceof Seer && !(player.role instanceof Fool));
     startMessageText = `Ты ${this.roleName}! Ты знаешь, кто настоящий провидец, а не дурак... В общем это ` +
-        `твоя единственная функция. ${this.seers ? this.seers.join(', ') + ' - провид' +
-            (this.seers.length === 1 ? 'ец!' : 'цы') :
+        `твоя единственная функция.\n${this.seers?.length ? this.seers.join(', ') + ' - провид' +
+            (this.seers.length === 1 ? 'ец!' : 'цы.') :
             'Провидца нет!'}`
     weight = () => this.seers ? 6 : 2;
 }
