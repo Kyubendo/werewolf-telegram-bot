87965db106b1c3b1bebc1a06919c95b255f05838
Kyubendo <kotigo111@gmail.com>
2021-10-24 13:40:26 +0300

    fix

---
+++ src/TgBot.ts
@@ -0,0 +1,10 @@
+import TelegramBot from "node-telegram-bot-api";
+
+export class TgBot extends TelegramBot {
+    sendMessage(chatId: TelegramBot.ChatId, text: string, options?: TelegramBot.SendMessageOptions): Promise<TelegramBot.Message> {
+        return super.sendMessage(chatId, text, {parse_mode: 'Markdown', ...options});
+    }
+    editMessageText(text: string, options?: TelegramBot.EditMessageTextOptions): Promise<TelegramBot.Message | boolean> {
+        return super.editMessageText(text, {parse_mode: 'Markdown', ...options});
+    }
+}
