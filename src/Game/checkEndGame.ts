177f4469f5e6fd7d839495285c3250121a0f17b9
Igor <kotigo111@gmail.com>
2021-10-28 17:39:15 +0300

    check end game

--- src/Game/checkEndGame.ts
+++ src/Game/checkEndGame.ts
@@ -12,24 +12,62 @@ import {
     Mason,
     Monarch, Oracle, Seer, SerialKiller, Traitor, Villager, WiseElder, Wolf, WoodMan
 } from "../Roles";
+import {GameStage} from "./Game";
 
-const villagers = [
+const villagers: Function[] = [
     ApprenticeSeer, Beholder, ClumsyGuy, Cursed, Drunk, GuardianAngel, Gunner, Harlot, Mason, Monarch, Oracle, Seer,
     Traitor, Villager, WiseElder, WoodMan
 ]
-const wolfTeam = [Wolf,]
-const evil = [Wolf, SerialKiller]
-const killers = [Wolf, Gunner, SerialKiller]
+const wolfTeam: Function[] = [Wolf,]
+const evil: Function[] = [Wolf, SerialKiller]
+const nonWolfKillers: Function[] = [SerialKiller]
 
 
-export const checkEndGame = (players: Player[]): Player[] => {
+export const checkEndGame = (players: Player[], stage: GameStage): undefined | Player[] => {
+    const alivePlayers = players.filter(p => p.isAlive)
 
-    if (!players.find(p => p.isAlive && evil.find(e => p.role instanceof e))) {
-        return players.filter(p => villagers.find(v => p instanceof v))
+    if (!alivePlayers.find(p => evil.find(e => p.role instanceof e))) {
+        return players.filter(p => villagers.find(v => p.role instanceof v))
     }
-    return []
 
-    // players.filter(p => killers.find(k => p instanceof k))
+    const aliveWolves = alivePlayers.filter(p => p.role instanceof Wolf)
+
+    alivePlayers.find(p => p.role instanceof Gunner && p.role.ammo) && nonWolfKillers.push(Gunner)
+    const aliveUniqueKillers = [...new Set(alivePlayers
+        .filter(p => nonWolfKillers.find(k => p.role instanceof k))
+        .map(p => p.role!.constructor))]
+    aliveWolves.length && aliveUniqueKillers.push(Wolf)
+
+    if (aliveUniqueKillers.length > 1) {
+        if (alivePlayers.length > 2) return undefined
+        else {
+            const wolf = players.find(p => p.role instanceof Wolf)
+            const serialKiller = players.find(p => p.role instanceof SerialKiller)
+            const gunner = players.find(p => p.role instanceof Gunner)
+            // const cowboy = players.filter(p => p.role instanceof Cowboy)
+            // const puppetMaster = players.filter(p => p.role instanceof PuppetMaster)
+
+            // if(puppetMaster) return puppetMaster
+            if (wolf && serialKiller) return [serialKiller]
+            if ((wolf || serialKiller) && gunner) {
+                if (stage === 'night') return players.filter(p => villagers.find(v => p.role instanceof v))
+                return [wolf ?? serialKiller!]
+            }
+            // if(cowboy && serialKiller) return []
+            // if(cowboy && wolf) return [Math.random()>.3 wolf:cowboy]
+        }
+    }
+
+    if (aliveWolves.length * 2 >= alivePlayers.length) {
+        return players.filter(p => p.role instanceof Wolf)
+    }
+
+    if (alivePlayers.length < 3) {
+        const evilPlayer = players.find(p => evil.find(e => p.role instanceof e))
+        return evilPlayer ? [evilPlayer] : undefined
+    }
+
+    return undefined
 }
 
 
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,5 @@
 import {config} from "dotenv";
+
 config({path: __dirname + '/./../.env'})
 import {Player} from "./Player/Player";
 import {Game} from "./Game/Game";
@@ -9,14 +10,23 @@ import {checkEndGame} from "./Game/checkEndGame";
 
 const bot = new TelegramBot(process.env.BOT_TOKEN!, {polling: true});
 
-const players =[...Array(5)].map((_, i) => new Player({id: 0, first_name: 'p' + i, is_bot: false,}))
+const rolePool = [Villager, Villager, SerialKiller, SerialKiller,Wolf, Lycan,Fool]
+const players = [...Array(rolePool.length)].map((_, i) => new Player({id: 0, first_name: 'p' + i, is_bot: false,}))
 
 const game = new Game('classic', bot, players, 0, 0)
 RoleBase.game = game
-
-const rolePool = [Villager, Villager, SerialKiller, Lycan, Fool]
-
 game.players.map((player, i) => player.role = new rolePool[i](player))
 
 
-console.log(checkEndGame(game.players).map(p=>p.name))
+
+//
+// const sk = game.players.find(p => p.role instanceof SerialKiller)
+// if (sk) sk.isAlive = false
+//
+// const wolf = game.players.find(p => p.role instanceof Wolf)
+// if (wolf) wolf.isAlive = false
+
+
+
+// console.log(checkEndGame(game.players).map(p => p.name))
+console.log(checkEndGame(game.players))
