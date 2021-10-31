37bb3dafffc4cce71e394045a754b179ab83afc3
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-31 20:23:39 +0200

    fix

--- src/Game/roleAssign.ts
+++ src/Game/roleAssign.ts
@@ -6,11 +6,13 @@ export const assignRoles = (game: Game) => {
     RoleBase.game = game;
     const players = game.players
     const rolePool = [
+        Roles.GuardianAngel, Roles.AlphaWolf, Roles.Oracle, Roles.WiseElder, Roles.GuardianAngel,
+        Roles.Villager,
         Roles.Monarch,Roles.Martyr, Roles.Villager, Roles.SerialKiller, Roles.GuardianAngel, Roles.Villager,
 
         Roles.Villager, Roles.ClumsyGuy, Roles.Cursed, Roles.Traitor, Roles.WoodMan, Roles.Mason,
         Roles.Beholder, // Passive Villagers
-      
+
         Roles.Seer, Roles.Fool, Roles.Monarch, Roles.Harlot, Roles.Oracle, Roles.Gunner, Roles.GuardianAngel,
         Roles.WiseElder, Roles.Sandman, Roles.Blacksmith, Roles.WildChild, // Active Villagers
 
