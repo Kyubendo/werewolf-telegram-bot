335630adfeba8b9857c8301a0b242b3e7419bc9b
Artyom Procenko <supermetalkiller@gmail.com>
2021-10-31 22:05:04 +0200

    fix

--- src/Game/roleAssign.ts
+++ src/Game/roleAssign.ts
@@ -33,16 +33,30 @@ export const assignRoles = (game: Game) => {
         Suicide, Thief, Necromancer, // Other
     ]
 
+    const testPool = [
+
+        Villager, ClumsyGuy, Cursed, WoodMan, Mason,
+        Beholder, // Passive Villagers
+
+        Seer, Monarch, Fool, Harlot, Oracle, Gunner, GuardianAngel,
+        WiseElder, Sandman, Blacksmith, WildChild, // Active Villagers
+
+        Suicide, Thief, Necromancer, // Other
+    ]
+
     let balanced = false
     do {
         const availableKillers = [...killersPool]
         arrayShuffle(availableKillers)
 
         let rolePool = [...villagersPool]
+        console.log(11)
 
         const evilCount = Math.floor((players.length - 4) / 2) + +(Math.random() >= .5)
         const evils = [...Array(evilCount)].map(() => availableKillers.pop())
         evils.find(e => e instanceof Wolf) && rolePool.push(...wolfNeededRoles)
+
+        console.log(12)
 
         arrayShuffle(rolePool)
 
@@ -53,17 +67,24 @@ export const assignRoles = (game: Game) => {
         evils.forEach(e => e && rolePool.unshift(e))
         rolePool = rolePool.slice(0, players.length)
         arrayShuffle(rolePool)
+        console.log(13)
 
         const currentRoles = players.map((player, i) => player.role = new rolePool[i](player))
+        console.log(14)
 
         const weight = Math.abs(currentRoles.reduce((a, c) => a + c.weight(), 0))
         const currentEvilCount = currentRoles.filter(r => evilPool.find(e => r instanceof e)).length
 
+        console.log(15)
         balanced = currentEvilCount <= players.length / 2 - 1
             && weight <= players.length / 3
 
     } while (!balanced)
 
+    //const currentRoles = players.map((player, i) => player.role = new testPool[i](player))
+
+
+    console.log(100)
     players.forEach(player => player.role && game.bot.sendMessage(
         player.id,
         player.role.roleIntroductionText() + player.role.startMessageText())
