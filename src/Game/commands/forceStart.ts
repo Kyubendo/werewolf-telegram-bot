2f04ea020544572a87d85d3303decf4509e28386
Kyubendo <kotigo111@gmail.com>
2021-11-01 01:28:35 +0200

    start on 6 players

--- src/Game/commands/forceStart.ts
+++ src/Game/commands/forceStart.ts
@@ -6,6 +6,10 @@ export const forceStart = (bot: TelegramBot, state: State) => {
     bot.onText(/\/force_start/, msg => {
         const game = state.game
         if (!game || game.stage) return;
+        if (game.players.length < 6) {
+            bot.sendMessage(game.chatId, 'Нельзя начать игру на 5 и меньше игроков!')
+            return;
+        }
         gameStart(bot, game)
     })
 }
