41b29eb162d8b95517b9d3c5589978b5fa405f85
Artyom Procenko <supermetalkiller@gmail.com>
2021-11-02 16:30:24 +0200

    fix

--- src/Roles/Villagers/Beholder.ts
+++ src/Roles/Villagers/Beholder.ts
@@ -9,11 +9,11 @@ export class Beholder extends RoleBase {
         .filter(player => player.role instanceof Seer && !(player.role instanceof Fool))
         .map(player => highlightPlayer(player))
 
-    stealMessage = `\n${this.seers?.length === 0
-            ? 'Провидца нет!'
+    stealMessage = this.seers?.length === 0
+            ? '\nПровидца нет!'
             : this.seers?.length === 1
-                ? `${this.seers[0]} может спасти народ, защищай его!`
-                : 'Провидцы: ' + this.seers?.join(', ')}\``
+                ? `\n${this.seers[0]} может спасти народ, защищай его!`
+                : '\nПровидцы: ' + this.seers?.join(', ');
 
     startMessageText = () => `Ты знаешь, кто настоящий провидец, а не дурак... В общем это ` +
         'твоя единственная функция.' + this.stealMessage;
