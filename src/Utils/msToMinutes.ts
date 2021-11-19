b4e8f7c9d6de8f3a8d4a74c2d37e5a7fbc6c9390
Igor <kotigo111@gmail.com>
2021-11-19 15:57:41 +0200

    fix time format

--- src/Utils/msToMinutes.ts
+++ src/Utils/msToMinutes.ts
@@ -1,4 +1,5 @@
 export const msToMinutes = (ms: number) => {
     const seconds = Math.floor(ms / 1000)
-    return `${Math.floor(seconds / 60)}:${seconds % 60}`
+    const [m, s] = [Math.floor(seconds / 60), seconds % 60]
+    return `${m > 9 ? m : '0' + m}:${s > 9 ? s : '0' + s}`
 }
