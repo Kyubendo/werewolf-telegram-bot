19f5c6a81f3a33f00cfee5aeeac0124495a11d83
Igor <kotigo111@gmail.com>
2021-11-22 11:35:19 +0200

    fix

--- src/Utils/endGameMessage.ts
+++ src/Utils/endGameMessage.ts
@@ -31,7 +31,7 @@ export const endGameMessage: { [type in Win]: { text: string, gif: string } } = 
         gif: 'https://media.giphy.com/media/rtgu0LxGT0R56/giphy.gif'
     },
     arsonist: {
-        text: 'Слышно только потрескивание пламени — вся деревня в огне. ' +
+        text: 'Слышно лишь потрескивание пламени — вся деревня в огне. ' +
             'Только поджигатель сидит посреди села, довольно улыбаясь',
         gif: 'https://media.giphy.com/media/IguL9zRYV20X1QeKuV/giphy.gif'
     }
