1db303fc2046f88d4208ea9d22fa60e0c79e1741
Igor <kotigo111@gmail.com>
2021-11-30 15:12:33 +0200

    pacifist release

--- src/Game/roleAssign.ts
+++ src/Game/roleAssign.ts
@@ -28,7 +28,7 @@ export const assignRoles = async (game: Game) => {
         Villager,
         ClumsyGuy, Cursed, WoodMan, Mason, Beauty, Drunk, Beholder, Princess, // Passive Villagers
 
-        Seer, Monarch, Mayor, Fool, Harlot, Oracle, Gunner, GuardianAngel, Cupid, //Pacifist,
+        Seer, Monarch, Mayor, Fool, Harlot, Oracle, Gunner, GuardianAngel, Cupid, Pacifist,
         WiseElder, Sandman, Blacksmith, WildChild, Detective, Martyr,// Active Villagers
 
         Suicide, Thief, Necromancer, Doppelganger// Other
