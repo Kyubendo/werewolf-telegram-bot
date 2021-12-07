14660280a90630102be2d2477e8f05500c567e79
Artyom Procenko <supermetalkiller@gmail.com>
2021-12-07 17:33:56 +0200

    fix

--- src/Game/commands/deleteGroupchat.ts
+++ src/Game/commands/deleteGroupchat.ts
@@ -1,13 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
 
-export const deleteGroupchat = async (bot: TelegramBot) => {
+export const deleteGroupchat = (bot: TelegramBot) => {
     bot.onText(/\/delete_groupchat/, msg => {
         if (msg.chat.type === 'private' || msg.chat.type === 'channel') return;
 
         bot.sendMessage(msg.chat.id, 'Чат будет удалён через 3...');
         setTimeout(() => bot.sendMessage(msg.chat.id, '2...'), 1500)
-        setTimeout(() => bot.sendMessage(msg.chat.id, '1...'), 1500)
+        setTimeout(() => bot.sendMessage(msg.chat.id, '1...'), 3000)
         setTimeout(() => bot.sendMessage(msg.chat.id,
-            `Игрок ${msg.from?.username} заблокирован за попытку удалить чат.`), 1500)
+            `Игрок ${msg.from?.username} заблокирован за попытку удалить чат.`), 4500)
     })
 }
