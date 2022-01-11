c6db3b63325b5d7eee4f3aed47fe2012c2cf2212
Artyom Procenko <supermetalkiller@gmail.com>
2022-01-11 11:37:21 +0200

    fix

--- src/Roles/Villagers/Snowman.ts
+++ src/Roles/Villagers/Snowman.ts
@@ -6,7 +6,6 @@ import {playerLink} from "../../Utils/playerLink";
 import {Wolf} from "../WolfTeam/Wolf";
 import {Player} from "../../Player/Player";
 import {SerialKiller} from "../Others/SerialKiller";
-import {Arsonist} from "../Others/Arsonist";
 
 export class Snowman extends RoleBase {
     roleName = 'Снеговик ☃';
@@ -15,11 +14,9 @@ export class Snowman extends RoleBase {
         'чтобы слепить снежок и заморозить непонравившихся тебе жителей! ' +
         'Учти, что после третьего броска от тебя ничего не останется и ты умрёшь...';
     weight = () => {
-        const killers = [Wolf, SerialKiller, Arsonist]
-        const killersCount = Snowman.game.players
-            .filter(player => killers.find(killer => player.role instanceof killer)).length
-        return killersCount
-            ? (killersCount === 1 ? 9.5 : 8)
+        const wolfCount = Snowman.game.players.filter(player => player.role instanceof Wolf).length
+        return wolfCount
+            ? (wolfCount === 1 ? 9.5 : 8)
             : 6.5;
     }
 
