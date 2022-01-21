23eeebe6b7acd78c2961284b27ef537f537e1105
Igor <kotigo111@gmail.com>
2022-01-21 17:17:00 +0200

    fix infected

--- src/Game/checkEndGame.ts
+++ src/Game/checkEndGame.ts
@@ -26,6 +26,8 @@ export const checkEndGame = (players: Player[], stage: GameStage): undefined | {
     const aliveEvilPlayer = alivePlayers.find(p => evil.find(e => p.role instanceof e))
     const aliveJackPlayers = alivePlayers.filter(player => player.role instanceof JackOLantern);
 
+    const infected = players.find(p => p.infected)
+
     if (alivePlayers.length === 2 && alivePlayers[0].lover === alivePlayers[1]) {
         return {winners: alivePlayers.filter(player => player.lover), type: 'lovers'}
     }
@@ -36,7 +38,10 @@ export const checkEndGame = (players: Player[], stage: GameStage): undefined | {
     }
 
     if (!aliveEvilPlayer) {
-        if (villagersTeamPlayers.find(p => p.isAlive)) return {winners: villagersTeamPlayers, type: 'villagers'}
+        if (!infected && villagersTeamPlayers.find(p => p.isAlive)) return {
+            winners: villagersTeamPlayers,
+            type: 'villagers'
+        }
         else return {winners: [], type: 'nobody'}
     }
 
@@ -61,7 +66,7 @@ export const checkEndGame = (players: Player[], stage: GameStage): undefined | {
 
             if (wolf && serialKiller) return {winners: [serialKiller], type: 'serialKiller'}
             if ((wolf || serialKiller || arsonist) && gunner) {
-                if (stage === 'day') {
+                if (stage === 'day' && !infected) {
                     aliveEvilPlayer.isAlive = false
                     return {winners: villagersTeamPlayers, type: 'villagers'} // custom gunner win
                 }
